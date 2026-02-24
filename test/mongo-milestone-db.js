81970ea420a66e0a4b2e7107efb35293eb6fb8ac
Dawid Kisielwski <91346346+dawidreedsy@users.noreply.github.com>
2026-02-24 11:51:56 +0100

    ✨ Add mongodb driver version 7 and mongodb version 8

--- test/mongo-milestone-db.js
+++ test/mongo-milestone-db.js
@@ -11,6 +11,7 @@ const MONGO_URL = process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test'
   'mongodb4',
   'mongodb5',
   'mongodb6',
+  'mongodb7',
 ].forEach((driver) => {
   const mongodb = require(driver);
 
